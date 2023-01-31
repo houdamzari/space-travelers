f3662f72085b56db28b4cabf2592c6c455bb6a7c
houdamzari <houdamzari1999@gmail.com>
2023-01-31 17:06:01 +0100

    Fix linter issues

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 function Rockets(props) {
-  const state = 
+  
   return <div>Hello From Rockets Page</div>;
 }
 
