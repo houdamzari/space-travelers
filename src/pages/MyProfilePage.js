21f5bcfac9720dea59ab77d2c5132d1c4e542625
Abu Raihan <raihan2bd.official@gmail.com>
2023-01-31 13:49:30 +0600

    Add basic markups in my profile

--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -1,10 +1,12 @@
+import classes from './MyProfilePage.module.css';
+
 const DUMMY_ROCKETS = [
   {
     id: '1',
     name: 'Starship',
     description:
       'Starship and Super Heavy Rocket represent a fully reusable transportation system designed to service all Earth orbit needs as well as the Moon and Mars. This two-stage vehicle — composed of the Super Heavy rocket (booster) and Starship (ship) — will eventually replace Falcon 9, Falcon Heavy and Dragon.',
-    reserved: true,
+    reserved: false,
   },
   {
     id: '2',
@@ -46,12 +48,40 @@ const DUMMY_MISSIONS = [
   },
 ];
 
-const reservedRockets = DUMMY_ROCKETS.filter(
-  (rocket) => rocket.reserved !== true
-);
-const reservedMissions = DUMMY_MISSIONS.filter(
-  (mission) => mission.reserved !== true
-);
-const MyProfilePage = () => <h2>Hello From MyProfile Page</h2>;
+const MyProfilePage = () => {
+  const reservedMissions = DUMMY_MISSIONS.filter(
+    (mission) => mission.reserved !== false
+  );
+  const reservedRockets = DUMMY_ROCKETS.filter(
+    (rocket) => rocket.reserved !== false
+  );
+
+  let missionsContent = <p>No Missions Found! Please join a new one!</p>;
+  if (reservedMissions.length > 0) {
+    missionsContent = reservedMissions.map((mission) => (
+      <li key={mission.mission_id}>{mission.mission_name}</li>
+    ));
+  }
+
+  let rocketsContent = <p>No Rockets Found! Please reserved a new one!</p>;
+  if (reservedRockets.length > 0) {
+    rocketsContent = reservedRockets.map((rocket) => (
+      <li key={rocket.id}>{rocket.name}</li>
+    ));
+  }
+
+  return (
+    <section className={classes.my_profile_section}>
+      <div className={classes.my_missions}>
+        <h3>My Missions</h3>
+        <ul className={classes.profile_items}>{missionsContent}</ul>
+      </div>
+      <div className={classes.my_rockets}>
+        <h3>My Rockets</h3>
+        <ul className={classes.profile_items}>{rocketsContent}</ul>
+      </div>
+    </section>
+  );
+};
 
 export default MyProfilePage;
