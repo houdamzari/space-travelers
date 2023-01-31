1d324b4a7b75c9b1088132a4693275eff687a260
houdamzari <houdamzari1999@gmail.com>
2023-01-31 10:24:39 +0100

    Underline active nav items

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,32 +3,55 @@ import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/space-travelers.png";
 import classes from "./Header.module.css";
 
-const Header = () => (
-  <header className={classes.header}>
-    <nav className={classes.nav}>
-      <div className={classes.brand}>
-        <div className={classes.logo_img}>
-          <img src={logo} alt="Space Travlers" />
+const Header = () => {
+  let activeStyle = {
+    textDecoration: "underline",
+  };
+
+  let activeClassName = "underline";
+
+  return (
+    <header className={classes.header}>
+      <nav className={classes.nav}>
+        <div className={classes.brand}>
+          <div className={classes.logo_img}>
+            <img src={logo} alt="Space Travlers" />
+          </div>
+          <div className={classes.logo_txt}>
+            <Link to="/">
+              <h1>Space Travelers&apos; Hub</h1>
+            </Link>
+          </div>
         </div>
-        <div className={classes.logo_txt}>
-          <Link to="/">
-            <h1>Space Travelers&apos; Hub</h1>
-          </Link>
-        </div>
-      </div>
-      <ul className={classes.nav_items}>
-        <li>
-          <NavLink to="/">Rockets</NavLink>
-        </li>
-        <li>
-          <NavLink to="/missions">Missions</NavLink>
-        </li>
-        <li>
-          <NavLink to="/profile">My Profile</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+        <ul className={classes.nav_items}>
+          <li>
+            <NavLink
+              to="/"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/missions"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/profile"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
