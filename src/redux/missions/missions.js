c11ce3fbed34d70dd5f9eb052022b7f84f3a1943
Abu Raihan <raihan2bd.official@gmail.com>
2023-01-31 16:34:17 +0600

    Setup action and reducer to fetch the missions

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,4 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const fetchMishions = createAsyncThunk(
+  'missions/fetch-missions',
+  async () => {
+    const response = await axios.get('https://api.spacexdata.com/v3/missions');
+    if (response.status !== 200) return [];
+    const data = response.data.map((mission) => {
+      return {
+        mission_id: mission.mission_id,
+        mission_name: mission.mission_name,
+        description: mission.description,
+      };
+    });
+
+    return data;
+  }
+);
 
 const initialState = {
   missions: [],
@@ -8,4 +26,11 @@ const missions = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchMishions.fulfilled, (state, { payload }) => {
+      return { ...state, missions: [...payload] };
+    });
+  },
 });
+
+export default missions;
