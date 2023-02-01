0e6df72d6ba194ee9e27e14f4a6ad4bc62f31cd2
Abu Raihan <raihan2bd.official@gmail.com>
2023-02-01 17:54:35 +0600

    Test Header component with snapshot

---
+++ src/__test__/Header.test.js
@@ -0,0 +1,30 @@
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from '../components/layout/Header';
+
+describe('Test Header Component', () => {
+  test('header component should match with snapshot', () => {
+    const header = renderer
+      .create(
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      )
+      .toJSON();
+
+    expect(header).toMatchSnapshot();
+  });
+
+  test('should match with logo text', () => {
+    render(
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    );
+
+    const text = screen.getByText("Space Traveler's Hub", { exact: false });
+    expect(text).toBeInTheDocument();
+  });
+});
